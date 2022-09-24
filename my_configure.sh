2859f1221ee63068ec2718b3c3c660914c77882b
jacktang <jack@jack-arch2.com>
2022-09-24 21:52:36 +0800

    添加sanitize，准备查内存释放部分

--- my_configure.sh
+++ my_configure.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-./configure --with-debug --with-cc-opt='-g -O0 -fno-strict-aliasing' --add-module='mymodules/check_auth/' --add-module='mymodules/mycurl/'
+./configure --with-debug --with-cc-opt='-g -O0 -fno-strict-aliasing -fsanitize=address' --with-ld-opt='-fsanitize=address' --add-module='mymodules/check_auth/' --add-module='mymodules/mycurl/'
 
